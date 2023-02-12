4ce81660979ea76c9b7667bdf57803ef403a9ae4
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-13 01:36:53 +0900

    インポート更新

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import BREAKPOINT from "../../variables/breakpoint";
-import COLOR from "../../variables/color";
-import FONTFAMILY from "../../variables/font_family";
-import TEXT from "../../variables/texts";
+import BREAKPOINT from "../../../variables/breakpoint";
+import COLOR from "../../../variables/color";
+import FONTFAMILY from "../../../variables/font_family";
+import TEXT from "../../../variables/texts";
 
 const Title = () => {
   return <StyledText>SIMPLE TODO APP</StyledText>;
