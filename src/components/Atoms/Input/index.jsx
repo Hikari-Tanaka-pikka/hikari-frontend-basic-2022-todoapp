b5c4563ee1a214cefa1b67e7cf35d76ea24431f4
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-17 01:33:59 +0900

    Input

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import styled from "styled-components";
-import COLOR from "../../../variables/color";
-import TEXTS from "../../../variables/texts";
+import { useRef, useEffect } from "react";
 
-const Input = () => {
-  return <StyledText>This is example.</StyledText>;
+
+const Input = (props) => {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.value = props.defaltValue;
+    inputRef.current.focus();
+    inputRef.current.onBlur = () => {
+      onEditComplete(inputRef.current.value);
+    }
+    inputRef.current.onkeypress = (e) => {
+      if (e.key === 'Enter') {
+        onEditComplete(inputRef.current.value);
+    }
+  },[]});
+
+  return (
+    <>
+      <input
+        type="text"
+        ref={inputRef}
+      />
+    </>
+  );
 };
 export default Input;
-
-const StyledText = styled.div`
-  color: ${COLOR.WHITE};
-  ${TEXTS.S}
-`;
