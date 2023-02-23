e4486168947a16988775eb8bd4961f8f1359103f
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-02-23 12:58:00 +0900

    WIP

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -5,37 +5,43 @@ import AddTaskButton from "../../Atoms/AddTaskButton";
 import Task from "../../Molecules/Task";
 
 const TodoCard = () => {
-  const [taskList, setTask] = useState([]);
-  const onClick = () => {
+  const [taskList, setTaskList] = useState([]);
+  const addNewTask = () => {
     const newTask = {
       name: "",
       initializing: true,
     };
-    setTask(taskList.concat(newTask));
+    setTaskList(taskList.concat(newTask));
   };
-  const onTaskNameChange = (value, index) => {
+  const taskNameChange = (value, index) => {
     if (value === "") {
-      const notask = taskList;
+      const notask = [...taskList];
       notask.splice(index, 1);
-      setTask(notask);
+      setTaskList(notask);
     } else {
-      const addTask = taskList;
+      const addTask = [...taskList];
       addTask[index].name = value;
-      setTask(addTask);
+      setTaskList(addTask);
     }
+  };
+  const onTaskComplete = (index) => {
+    const completed = [...taskList];
+    completed.splice(index, 1);
+    setTaskList(completed);
   };
 
   return (
     <StyledWrapper>
-      <AddTaskButton onClick={onClick} />
+      <AddTaskButton onClick={addNewTask} />
       <StyledTaskList>
         <ol>
           {taskList.map((task, index) => (
             <Task
               key={index}
-              onTaskNameChange={(value) => onTaskNameChange(value, index)}
+              onEditComplete={(value) => taskNameChange(value, index)}
               taskName={task.name}
               defaultIsEditing={task.initializing}
+              onClick={onTaskComplete}
             />
           ))}
         </ol>
