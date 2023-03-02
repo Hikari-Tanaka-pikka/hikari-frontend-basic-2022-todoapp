e877f36fa174e10a59ca21a4fe816f3b7bf01a4c
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-03-02 23:02:16 +0900

    wip

---
+++ src/components/Organisms/index.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import AddTaskButton from "../../Atoms/AddTaskButton";
+import Task from "../../Molecules/Task";
+
+const TodoCard = () => {
+  const [taskList, setTaskList] = useState([]);
+  const addNewTask = () => {
+    const newTask = {
+      name: "",
+      initializing: true,
+    };
+    setTaskList(taskList.concat(newTask));
+  };
+  const taskNameChange = (value, index) => {
+    if (value === "") {
+      const notask = [...taskList];
+      notask.splice(index, 1);
+      setTaskList(notask);
+    } else {
+      const addTask = [...taskList];
+      addTask[index].name = value;
+      setTaskList(addTask);
+    }
+  };
+  const onTaskComplete = (index) => {
+    const completed = [...taskList];
+    completed.splice(index, 1);
+    setTaskList(completed);
+  };
+
+  return (
+    <StyledWrapper>
+      <AddTaskButton onClick={addNewTask} />
+      <StyledTaskList>
+        <ol>
+          {taskList.map((task, index) => (
+            <Task
+              key={index}
+              onEditComplete={(value) => taskNameChange(value, index)}
+              taskName={task.name}
+              defaultIsEditing={task.initializing}
+              onClick={onTaskComplete}
+            />
+          ))}
+        </ol>
+      </StyledTaskList>
+    </StyledWrapper>
+  );
+};
+export default TodoCard;
+
+const StyledWrapper = styled.div`
+  width: 100%;
+`;
+
+const StyledTaskList = styled.div`
+  width: 100%;
+`;
