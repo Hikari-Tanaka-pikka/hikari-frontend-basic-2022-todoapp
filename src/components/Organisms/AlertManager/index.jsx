09d053f53ccdbd1c3e61368e811134b38a966f83
pg <h.tanaka.22.7.575@4nonome.com>
2023-05-11 00:20:09 +0900

    WIP

---
+++ src/components/Organisms/AlertManager/index.jsx
@@ -0,0 +1,23 @@
+import { useAlertHandlerContext } from "../../../contexts/alert_handler";
+import Alert from "../../Atoms/Alert ";
+import React, { useEffect } from "react";
+
+const AlertManager = () => {
+  const AlertHandlerContext = useAlertHandlerContext();
+  useEffect(() => {
+    if (AlertHandlerContext.isActive == true) {
+      console.log("aaa");
+      setTimeout(() => {
+        AlertHandlerContext.closeAlert();
+      }, 5000);
+    }
+  }, [AlertHandlerContext]);
+  return (
+    <Alert
+      alertMessage={AlertHandlerContext.alertMessage}
+      isActive={AlertHandlerContext.isActive}
+    />
+  );
+};
+
+export default AlertManager;
